5f070ccff4aff1fed3c5de921b3955adc01032e9
Lijo M Loyid <lijomloyid@gmail.com>
2020-04-24 02:08:08 +0530

    Initial

---
+++ lijo/lijo/src/main/java/com/lijo/lijo/LijoApplication.java
@@ -0,0 +1,13 @@
+package com.lijo.lijo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LijoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LijoApplication.class, args);
+	}
+
+}
